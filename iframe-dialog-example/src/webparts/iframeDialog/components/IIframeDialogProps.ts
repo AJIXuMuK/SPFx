17b64be3a33d9d1bfe203274f0a0060e2f748ef0
Alex Terentiev <aleksei.terentiev@gmail.com>
2018-03-23 09:42:27 -0700

    pnp react controls iframe sample

---
+++ iframe-dialog-example/src/webparts/iframeDialog/components/IIframeDialogProps.ts
@@ -0,0 +1,3 @@
+export interface IIframeDialogProps {
+  description: string;
+}
---
+++ iframe-dialog-example/src/webparts/iframeDialog/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IIframeDialogWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'IframeDialogWebPartStrings' {
+  const strings: IIframeDialogWebPartStrings;
+  export = strings;
+}
