fc772c3b83ac4ffdb94c1d3063ff1227013457de
= <=>
2019-09-11 10:25:24 -0700

    example on how to close iframe from button inside iframe

--- iframe-dialog-app-customizer/src/components/DialogLauncher.tsx
+++ iframe-dialog-app-customizer/src/components/DialogLauncher.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { PrimaryButton } from 'office-ui-fabric-react/lib/Button';
+import { PrimaryButton, DefaultButton } from 'office-ui-fabric-react/lib/Button';
 import { IFrameDialog } from '@pnp/spfx-controls-react/lib/IFrameDialog';
 import { DialogType } from 'office-ui-fabric-react/lib/Dialog';
 //import styles from './DialogLauncher.module.scss';
@@ -25,6 +25,7 @@ export class DialogLauncher extends React.Component<IDialogLauncherProps, IDialo
     return (
       <div>
         <PrimaryButton text="Click me, please!" onClick={() => { this.onButtonClick(); }} />
+        <DefaultButton text="Close iframe!" onClick={() => { this.onCloseClick(); }} />
         {this.state.isDlgOpen &&
           <IFrameDialog
             url={this.props.url}
@@ -33,6 +34,21 @@ export class DialogLauncher extends React.Component<IDialogLauncherProps, IDialo
             height={'800px'}
             modalProps={{
               isBlocking: true
+            }}
+            iframeOnLoad={iframe => {
+              const windowClose = iframe.contentWindow.close;
+              const windowClosingEvent = new Event('closeWindow');
+
+              iframe.addEventListener('closeWindow', () => {
+                this.setState({
+                  isDlgOpen: false
+                });
+              });
+
+              iframe.contentWindow.close = () => {
+                iframe.dispatchEvent(windowClosingEvent);
+                windowClose();
+              };
             }}
             dialogContentProps={{
               type: DialogType.close,
@@ -52,11 +68,17 @@ export class DialogLauncher extends React.Component<IDialogLauncherProps, IDialo
     this.setState({
       isDlgOpen: true
     });
-  }
+  };
+
+  private onCloseClick = () => {
+    if (window.parent !== window) {
+      window.close();
+    }
+  };
 
   private onDlgDismiss = () => {
     this.setState({
-    isDlgOpen: false
-  });
-  }
+      isDlgOpen: false
+    });
+  };
 }
